13c2cfa2433ba89dd6024ebe52f1fc20b22dde8b
menrma <menrma@NB134.BL-DOM.ZZ>
2016-06-17 12:46:31 +0200

    Anlegen Package dijkstra Klassen "Kanten", "Knoten", "Adjazenzliste" und "Dijkstra" implementieren

---
+++ src/dijkstra/Kanten.java
@@ -0,0 +1,46 @@
+package dijkstra;
+
+public class Kanten {
+	
+	private double distanz;
+	private String label;
+	Knoten[] knoten = new Knoten[2];
+	
+	//Setter und Getter
+	public void setDistanz(double distanz){
+		//Keine negativen Werte erlauben
+		if(distanz > 0){
+			this.distanz = distanz;
+		}
+		else{
+			//Error
+		}
+	}
+	
+	public double getDistanz(){
+		return distanz;
+	}
+	
+	public void setLabel(String label){
+		this.label = label;
+	}
+	
+	public String getLabel(){
+		return label;
+	}
+	
+	public void setKnoten(Knoten[] knoten){
+		if(knoten.length == 2){
+			this.knoten = knoten;
+		}
+		else
+		{
+			//Error
+		}
+	}
+	
+	public Knoten[] getKnoten(){
+		return knoten;
+	}
+
+}
