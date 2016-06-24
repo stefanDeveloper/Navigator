31115f995ece4812423a6c398d8821e6f867b7b9
stefanDeveloper <stefan-machmeier@hotmail.de>
2016-06-24 08:54:11 +0200

    Change UI

--- src/dhbw/navigator/io/Menufx.java
+++ src/dhbw/navigator/io/Menufx.java
@@ -1,16 +1,11 @@
 package dhbw.navigator.io;
 
 import java.io.IOException;
-
 import dhbw.navigator.start.StartNavigator;
-import dhbw.navigator.views.InfoViewController;
 import dhbw.navigator.views.MainController;
 import dhbw.navigator.views.RootLayoutController;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
 public class Menufx {
 	private StartNavigator startNavigator;
@@ -34,28 +29,4 @@ public class Menufx {
 			e.printStackTrace();
 		}
 	}
-	
-	public void viewInfoLabel(){
-		try {
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(InfoViewController.class.getResource("InfoViewWindow.fxml"));
-			AnchorPane pane = (AnchorPane) loader.load();
-			
-			Stage infoStage = new Stage();
-			infoStage.setTitle("Info");
-			infoStage.setResizable(false);
-			infoStage.initModality(Modality.WINDOW_MODAL);
-            infoStage.initOwner(this.startNavigator.getPrimaryStage());
-            Scene scene = new Scene(pane);
-            infoStage.setScene(scene);
-
-            InfoViewController controller = loader.getController();
-            controller.setStage(infoStage);
-
-            infoStage.showAndWait();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
 }
