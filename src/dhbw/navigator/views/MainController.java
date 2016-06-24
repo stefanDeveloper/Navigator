fa0b6440657c8432e93b929a00c38297d3eb5e51
Konrad Mueller <i15019@hb.dhbw-stuttgart.de>
2016-06-24 15:24:46 +0200

    New test data

--- src/dhbw/navigator/views/MainController.java
+++ src/dhbw/navigator/views/MainController.java
@@ -1,9 +1,17 @@
 package dhbw.navigator.views;
 
+import dhbw.navigator.generated.Osm;
+import dhbw.navigator.implementation.Parser;
+import dhbw.navigator.interfaces.IParser;
 import dhbw.navigator.io.Menufx;
+import dhbw.navigator.models.Node;
+import dhbw.navigator.utility.Map;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class MainController {
 	private Menufx menufx;
@@ -34,7 +42,10 @@ public class MainController {
 	@FXML
 	public void initialize(){
 		this.gc = mapView.getGraphicsContext2D();
-		
+		IParser parser = new Parser();
+		Osm test = (Osm) parser.parseFile(new File("Testdata/export.xml"));
+		ArrayList<Node> nodes = parser.getNodes(test);
+		Map.drawMap(gc, nodes);
 		
 	}
 
