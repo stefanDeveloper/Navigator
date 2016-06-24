ad145e37c9634016876f63925377d2cc38f154b3
Stefan Machmeier <stefan-machmeier@outlook.com>
2016-06-24 13:12:13 +0200

    Change UI

---
+++ src/dhbw/navigator/utility/Map.java
@@ -0,0 +1,11 @@
+package dhbw.navigator.utility;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class Map {
+	
+	public static void drawMap(GraphicsContext graphic){
+		
+	}
+
+}
--- src/dhbw/navigator/views/MainController.java
+++ src/dhbw/navigator/views/MainController.java
@@ -1,16 +1,41 @@
 package dhbw.navigator.views;
 
 import dhbw.navigator.io.Menufx;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 
 public class MainController {
 	private Menufx menufx;
+	
+	@FXML
+	private Canvas mapView;
 
+	private GraphicsContext gc;
+
+	public Canvas getMap() {
+		return this.mapView;
+	}
+
+	public void setMap(Canvas mapView) {
+		this.mapView = mapView;
+	}
+	
 	public Menufx getMenufx() {
 		return this.menufx;
 	}
 
 	public void setMenufx(Menufx menufx) {
 		this.menufx = menufx;
+	}
+	
+	
+	
+	@FXML
+	public void initialize(){
+		this.gc = mapView.getGraphicsContext2D();
+		
+		
 	}
 
 }
