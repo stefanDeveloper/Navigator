610d86610ac68e3473561a6808eaa8496374c6a1
Stefan Machmeier <stefan-machmeier@outlook.com>
2016-06-27 10:55:49 +0200

    Modified UI

--- src/dhbw/navigator/views/RouteController.java
+++ src/dhbw/navigator/views/RouteController.java
@@ -1,22 +1,20 @@
 package dhbw.navigator.views;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.SortedSet;
-import java.util.TreeSet;
-import dhbw.navigator.generated.Osm;
-import dhbw.navigator.implementation.Parser;
-import dhbw.navigator.interfaces.IParser;
 import dhbw.navigator.io.Menufx;
-import dhbw.navigator.models.Node;
 import dhbw.navigator.utility.Utility;
+import dhbw.navigator.utility.UtilityViews;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
+/**
+ * Controller for RouteWindow.fxml
+ * @author Stefan
+ *
+ */
 public class RouteController {
 	private Menufx menufx;
 	private Stage stageRoute;
@@ -37,6 +35,15 @@ public class RouteController {
 	private Button startButton;
 	@FXML
 	private Button finishButton;
+
+	
+	public SortedSet<String> getNameOfJunctions() {
+		return nameOfJunctions;
+	}
+
+	public void setNameOfJunctions(SortedSet<String> nameOfJunctions) {
+		this.nameOfJunctions = nameOfJunctions;
+	}
 
 	public Stage getStageRoute() {
 		return stageRoute;
@@ -65,17 +72,6 @@ public class RouteController {
 
 	@FXML
 	public void initialize() {
-		IParser parser = new Parser();
-		Osm test = (Osm) parser.parseFile(new File("Testdata/export.xml"));
-		ArrayList<Node> name = parser.getNodes(test);
-		
-		//Add Name of Junction to SortedSet
-		this.nameOfJunctions = new TreeSet<>();
-		for (Node n : name) {
-			if (n.getIsJunction() == true)
-				this.nameOfJunctions.add(n.getName());
-		}
-		
 		//Add AutoComplete to TextField with ContextMenu
 		Utility.AutoComplete(this.startTextField, this.nameOfJunctions, this.entriesPopupStart);
 		Utility.AutoComplete(this.finishTextField, this.nameOfJunctions, this.entriesPopupAim);
@@ -92,11 +88,16 @@ public class RouteController {
 	@FXML
 	private void handleOk() {
 		//Check if Name of TextFields are in the SortedSet
-		if (this.nameOfJunctions.contains(this.startTextField.getText()) &&
-				this.nameOfJunctions.contains(this.finishTextField.getText())){
-			
+		if (!this.nameOfJunctions.contains(this.startTextField.getText())){
+			UtilityViews.Error("Startpunkt ist nicht in der Liste vorhanden!\n"
+					+ "Bitte anderen Punkt wählen!");
+		} else if (!this.nameOfJunctions.contains(this.finishTextField.getText())){
+			UtilityViews.Error("Endpunkt ist nicht in der Liste vorhanden!\n"
+					+ "Bitte anderen Punkt wählen!");
 		} else {
-			
+			this.menufx.setStartTextField(this.startTextField);
+			this.menufx.setFinishTextField(this.finishTextField);
+			this.stageRoute.close();
 		}
 	}
 	
@@ -109,12 +110,22 @@ public class RouteController {
 	}
 	
 	/**
-	 * Clear TextField
+	 * Clear Start TextField
 	 */
 	@FXML
 	private void handleButtonStart(){
 		this.startTextField.clear();
-		
+		this.startTextField.requestFocus();
 	}
+	
+	/**
+	 * Clear Finish TextField
+	 */
+	@FXML
+	private void handleButtonFinish(){
+		this.finishTextField.clear();
+		this.finishTextField.requestFocus();
+	}
+	
 	
 }
