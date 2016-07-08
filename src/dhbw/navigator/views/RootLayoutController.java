99a93c38eb94c7dc59167a788af5fbb767b273e6
stefanDeveloper <stefan-machmeier@hotmail.de>
2016-07-08 11:46:44 +0200

    Add Comments

--- src/dhbw/navigator/views/RootLayoutController.java
+++ src/dhbw/navigator/views/RootLayoutController.java
@@ -3,30 +3,31 @@ package dhbw.navigator.views;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.File;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-import dhbw.navigator.controles.*;
+import dhbw.navigator.controles.AutoCompleteControle;
+import dhbw.navigator.controles.MapControle;
+import dhbw.navigator.controles.NodeInformationControle;
+import dhbw.navigator.controles.PathListingControle;
+import dhbw.navigator.controles.SlideBarControle;
 import dhbw.navigator.generated.Osm;
 import dhbw.navigator.implementation.Dijkstra;
 import dhbw.navigator.implementation.Parser;
 import dhbw.navigator.interfaces.IDijkstra;
 import dhbw.navigator.interfaces.IParser;
-import dhbw.navigator.models.Edge;
 import dhbw.navigator.models.Node;
 import dhbw.navigator.start.StartNavigator;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
-import net.aksingh.owmjapis.CurrentWeather;
 
 /**
- * Controller for RootLayout.fxml
+ * Controller for RootLayoutWindow.fxml
  * 
- * @author Stefan
+ * @author Stefan Machmeier, Manuela Leopold, Konrad Müller, Markus Menrath
  *
  */
 public class RootLayoutController implements PropertyChangeListener {
@@ -38,29 +39,32 @@ public class RootLayoutController implements PropertyChangeListener {
 	private NodeInformationControle originInformation = new NodeInformationControle();
 	private NodeInformationControle destinationInformation = new NodeInformationControle();
 	private PathListingControle pathListing = new PathListingControle();
-	private Button startButton = new Button("Start");
-
-
 	private StartNavigator start;
-
 	private ArrayList<Node> nodes;
-
 	@FXML
 	private StackPane primaryStackPane;
 	@FXML
-	private Button region;
+	private Button button;
 
 	public StartNavigator getStart() {
-		return this.start;
+		return start;
 	}
 
 	public void setStart(StartNavigator start) {
 		this.start = start;
 	}
 
+	/**
+	 * load Xml Data
+	 * 
+	 * @param Boolean,
+	 *            true for parse
+	 */
 	private void loadData(boolean parseData) {
 		IParser parser = new Parser();
+		// Check boolean
 		if (parseData) {
+			// Parse file and serialize it
 			Osm data = (Osm) parser.parseFile(new File("Testdata/germany.xml"));
 			nodes = parser.getNodes(data);
 			parser.serialize(nodes);
@@ -70,15 +74,19 @@ public class RootLayoutController implements PropertyChangeListener {
 		SortedSet<String> namesOfJunctions = new TreeSet<>();
 		for (Node n : nodes) {
 			if (n.getIsJunction() == true)
+				// Add names of Junction for Context Menu
 				namesOfJunctions.add(n.getName());
 		}
-		if(startPositionInput != null) startPositionInput.setNamesOfJunctions(namesOfJunctions);
-		if(destinationPositionInput != null) destinationPositionInput.setNamesOfJunctions(namesOfJunctions);
+		// Set name of junctions to AutoComplete
+		if (startPositionInput != null)
+			startPositionInput.setNamesOfJunctions(namesOfJunctions);
+		if (destinationPositionInput != null)
+			destinationPositionInput.setNamesOfJunctions(namesOfJunctions);
 
-		//Populate the view
-		this.primaryStackPane.getChildren().clear();
+		// Populate the view
+		primaryStackPane.getChildren().clear();
 		MapControle map = new MapControle(nodes, false);
-		this.addToCenter(map);
+		addToCenter(map);
 	}
 
 	@FXML
@@ -86,47 +94,35 @@ public class RootLayoutController implements PropertyChangeListener {
 		Platform.runLater(new Runnable() {
 			@Override
 			public void run() {
-				//Muss in anderne Thread ausgelagert werden
-				flapBar = new SlideBarControle(325, region,
-						startPositionInput,
-						destinationPositionInput,
-						originInformation,
-						destinationInformation,
-						pathListing);
+				// Initialize FlapBar and set
+				flapBar = new SlideBarControle(325, button, startPositionInput, destinationPositionInput,
+						originInformation, destinationInformation, pathListing);
 				start.getPrimaryBorder().setLeft(flapBar);
 				loadData(true);
-				region.fire();
+				button.fire();
 			}
 		});
-
-
+		// Add PropertyChangeListener to get
 		destinationPositionInput.addPropertyChangeListener("text", e -> {
-			if(e.getNewValue().equals(""))
-			{
-				destinationInformation.setNode(null);
+			if (e.getNewValue().equals("")) { // end node selected
+				destinationInformation.setNode(null); // return null?
 				setEndNode(null);
-			}
-			else
-			{
-				System.out.println("Ziel: " +e.getNewValue());
-				Node n = getNodeByName((String)e.getNewValue(), nodes);
-				if(n!=null) {
+			} else {
+				System.out.println("Ziel: " + e.getNewValue());
+				Node n = getNodeByName((String) e.getNewValue(), nodes);
+				if (n != null) {
 					setEndNode(n);
 				}
 			}
-        });
+		});
 		startPositionInput.addPropertyChangeListener("text", e -> {
-			if(e.getNewValue().equals(""))
-			{
+			if (e.getNewValue().equals("")) {
 				originInformation.setNode(null);
-				setStartNode(null);
-			}
-			else
-			{
-				System.out.println("Start: " +e.getNewValue());
-				Node n = getNodeByName((String)e.getNewValue(), nodes);
-				if(n!=null)
-				{
+				setStartNode(null); // reset/clear startNode
+			} else {
+				System.out.println("Start: " + e.getNewValue());
+				Node n = getNodeByName((String) e.getNewValue(), nodes);
+				if (n != null) {
 					setStartNode(n);
 
 				}
@@ -134,21 +130,20 @@ public class RootLayoutController implements PropertyChangeListener {
 		});
 	}
 
-	private Node getNodeByName(String name, ArrayList<Node> nodes)
-	{
-		for(Node n: nodes)
-		{
-			if(n.getName().equals(name)) return n;
+	private Node getNodeByName(String name, ArrayList<Node> nodes) {
+		for (Node n : nodes) {
+			if (n.getName().equals(name))
+				return n;
 		}
 		return null;
 	}
 
 	public void addToCenter(javafx.scene.Node node) {
-		this.primaryStackPane.getChildren().add(node);
+		primaryStackPane.getChildren().add(node);
 	}
 
 	public void removeToCenter(javafx.scene.Node node) {
-		this.primaryStackPane.getChildren().remove(node);
+		primaryStackPane.getChildren().remove(node);
 	}
 
 	/**
@@ -193,11 +188,9 @@ public class RootLayoutController implements PropertyChangeListener {
 		nodeChanged();
 	}
 
-	void nodeChanged()
-	{
-		if(startNode!=null && endNode !=null)
-		{
-			//Start dijkstra
+	void nodeChanged() {
+		if (startNode != null && endNode != null) {
+			// Start dijkstra
 			IDijkstra dijkstra = new Dijkstra();
 			ArrayList<Node> path = dijkstra.FindPath(startNode, endNode);
 			pathListing.setPath(path);
