a65d6354e552878160a13c752b0e66faf7c43a94
stefanDeveloper <stefan-machmeier@hotmail.de>
2016-08-01 10:09:03 +0200

    TODO's and change folder for data

--- src/dhbw/navigator/controles/AutoCompleteControle.java
+++ src/dhbw/navigator/controles/AutoCompleteControle.java
@@ -20,50 +20,51 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 
 public class AutoCompleteControle extends GridPane {
-	private Button clearButton; //Button can clear the TextField
-	private TextFieldAutoCompleteControle textField; //TextField with AutoComplete
-	private PropertyChangeSupport changes = new PropertyChangeSupport( this );
-	private String text; //Input-Text
+	private Button clearButton; // Button can clear the TextField
+	private TextFieldAutoCompleteControle textField; // TextField with
+														// AutoComplete
+	private PropertyChangeSupport changes = new PropertyChangeSupport(this);
+	private String text; // Input-Text //TODO ?????
 
-	//Constructor
+	// Constructor
 	public AutoCompleteControle(String labelText) {
 		Label label = new Label(labelText);
 		clearButton = new Button("X");
 		textField = new TextFieldAutoCompleteControle();
 		clearButton.setVisible(false);
-		setPadding(new Insets(5,0,0,5));
+		setPadding(new Insets(5, 0, 0, 5));
 
-		//Create four columns for the controls
+		// Create four columns for the controls
 		for (int i = 0; i < 4; i++) {
 			ColumnConstraints col = new ColumnConstraints();
 			getColumnConstraints().add(col);
 		}
-		
+
 		getColumnConstraints().get(0).setPercentWidth(15);
 		getColumnConstraints().get(1).setPercentWidth(70);
 		getColumnConstraints().get(2).setPercentWidth(2.5);
 		getColumnConstraints().get(3).setPercentWidth(12.5);
-		
-		//Add controls
+
+		// Add controls
 		add(label, 0, 0);
-		add(textField, 1 , 0);
+		add(textField, 1, 0);
 		add(clearButton, 3, 0);
 
-		//Listen to button action
-		//Clear TextField if button gets pushed
-		this.clearButton.setOnAction(event -> AutoCompleteControle.this.textField.setText(""));
-		//Listen to the input
-		//Show ClearButton if input is not empty
-		this.textField.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (AutoCompleteControle.this.textField.getText().length() > 0
-                    && !(AutoCompleteControle.this.textField.getText().equals(" "))) {
-                AutoCompleteControle.this.clearButton.setVisible(true);
+		// Listen to button action
+		// Clear TextField if button gets pushed
+		clearButton.setOnAction(event -> AutoCompleteControle.this.textField.setText(""));
+		// Listen to the input
+		// Show ClearButton if input is not empty
+		textField.textProperty().addListener((observable, oldValue, newValue) -> {
+			if (AutoCompleteControle.this.textField.getText().length() > 0
+					&& !(AutoCompleteControle.this.textField.getText().equals(" "))) {
+				AutoCompleteControle.this.clearButton.setVisible(true);
 
-            } else if (AutoCompleteControle.this.textField.getText().length() == 0) {
-                AutoCompleteControle.this.clearButton.setVisible(false);
-            }
-            changes.firePropertyChange("text", oldValue, newValue);
-        });
+			} else if (AutoCompleteControle.this.textField.getText().length() == 0) {
+				AutoCompleteControle.this.clearButton.setVisible(false);
+			}
+			changes.firePropertyChange("text", oldValue, newValue);
+		});
 	}
 
 	public void setNamesOfJunctions(SortedSet<String> namesOfJunctions) {
--- src/dhbw/navigator/controles/MapControle.java
+++ src/dhbw/navigator/controles/MapControle.java
@@ -1,26 +1,20 @@
 package dhbw.navigator.controles;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Stack;
 
-import com.sun.rowset.internal.Row;
 import dhbw.navigator.models.Edge;
 import dhbw.navigator.models.Node;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.layout.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 
 /**
- * MapControle
- * Control that can visualize a Node-Map with a Path
+ * MapControle Control that can visualize a Node-Map with a Path
  * 
  * @author Stefan Machmeier, Manuela Leopold, Konrad Müller, Markus Menrath
  *
@@ -50,45 +44,39 @@ public class MapControle extends StackPane {
 	private double xMulti;
 	private double yMulti;
 
-	public MapControle()
-	{
-		//Add mapLayer (map) and pathLayer (path)
-		getChildren().addAll(mapLayer,
-				startMarkLayer,
-				destinatinMarkLayer,
-				pathLayer
-		);
-		//Set default values for the layer
+	public MapControle() {
+		// Add mapLayer (map) and pathLayer (path)
+		getChildren().addAll(mapLayer, startMarkLayer, destinatinMarkLayer, pathLayer);
+		// Set default values for the layer
 		pathLayer.getGraphicsContext2D().setStroke(Color.AQUA);
 		pathLayer.getGraphicsContext2D().setLineWidth(3);
 
 		startMarkLayer.getGraphicsContext2D().setFill(Color.GREEN);
 		destinatinMarkLayer.getGraphicsContext2D().setFill(Color.RED);
 
-
-		//Listen to a change of the parent object
-		this.parentProperty().addListener(new ChangeListener<Parent>() {
+		// Listen to a change of the parent object
+		parentProperty().addListener(new ChangeListener<Parent>() {
 			@Override
 			public void changed(ObservableValue<? extends Parent> ov, Parent oldP, Parent newP) {
 				if (newP != null) {
-					//Set new parent
-					MapControle.this.parent = (BorderPane)getParent().getParent();
-					
-					//Update the graph
+					// Set new parent
+					parent = (BorderPane) getParent().getParent();
+
+					// Update the graph
 					updateGraph();
-					//Listen to the height and width of the parent
-					//and redraw the map
+					// Listen to the height and width of the parent
+					// and redraw the map
 					parent.heightProperty().addListener(new ChangeListener<Number>() {
 						@Override
 						public void changed(ObservableValue<? extends Number> observable, Number oldValue,
-											Number newValue){
+								Number newValue) {
 							updateGraph();
 						}
 					});
 					parent.widthProperty().addListener(new ChangeListener<Number>() {
 						@Override
 						public void changed(ObservableValue<? extends Number> observable, Number oldValue,
-											Number newValue) {
+								Number newValue) {
 							updateGraph();
 						}
 					});
@@ -97,41 +85,39 @@ public class MapControle extends StackPane {
 		});
 	}
 
-
-
 	private int movement = 25;
 
-	public void moveLeft(){
+	public void moveLeft() {
 		xOffset += movement;
 		updateGraph();
 	}
 
-	public void moveRight(){
+	public void moveRight() {
 		xOffset -= movement;
 		updateGraph();
 	}
 
-	public void moveDown(){
+	public void moveDown() {
 		yOffset -= movement;
 		updateGraph();
 	}
 
-	public void moveUp(){
+	public void moveUp() {
 		yOffset += movement;
 		updateGraph();
 	}
 
-	public void zoomIn(){
+	public void zoomIn() {
 		zoom += 0.1;
 		updateGraph();
 	}
 
-	public void zoomOut(){
+	public void zoomOut() {
 		zoom -= 0.1;
 		updateGraph();
 	}
-	
-	public void setPath(ArrayList<Node> pPathList){
+
+	public void setPath(ArrayList<Node> pPathList) {
 		pathList = pPathList;
 		updateGraph();
 	}
@@ -141,34 +127,33 @@ public class MapControle extends StackPane {
 		updateGraph();
 	}
 
-	public void setStart(Node n){
+	public void setStart(Node n) {
 		startNode = n;
 		updateGraph();
 	}
 
-	public void setDestinationNode(Node n){
+	public void setDestinationNode(Node n) {
 		destinationNode = n;
 		updateGraph();
 	}
-	
-	//Update the graph
-	//Calculate how to scale the map
+
+	// Update the graph
+	// Calculate how to scale the map
 	void updateGraph() {
 
 		double lon2;
 		double lat2;
 
-		if (this.parent != null && nodeList!=null) {
-			this.xSize = this.parent.getWidth();
-			this.ySize = this.parent.getHeight();
-			if (this.xSize < this.ySize) {
-				this.ySize = this.xSize;
+		if (parent != null && nodeList != null) {
+			xSize = parent.getWidth();
+			ySize = parent.getHeight();
+			if (xSize < ySize) {
+				ySize = xSize;
 			} else {
-				this.xSize = this.ySize;
+				xSize = ySize;
 			}
 
-			Node firstNode = this.nodeList.get(0);
-
+			Node firstNode = nodeList.get(0);
 
 			lat1 = firstNode.getLat().doubleValue();
 			lon1 = firstNode.getLon().doubleValue();
@@ -180,13 +165,13 @@ public class MapControle extends StackPane {
 
 			double compareLat;
 			double compareLon;
-			//Get the highest left and lowest right coordinate
-			for (Node n : this.nodeList) {
+			// Get the highest left and lowest right coordinate
+			for (Node n : nodeList) {
 				compareLat = n.getLat().doubleValue();
 				compareLon = n.getLon().doubleValue();
 				if (compareLat < lat1)
 					lat1 = compareLat; // compare is smaller
-				if (compareLon> lon1)
+				if (compareLon > lon1)
 					lon1 = compareLon; // compare is smaller
 
 				if (compareLat > lat2)
@@ -194,17 +179,16 @@ public class MapControle extends StackPane {
 				if (compareLon < lon2)
 					lon2 = compareLon; // compare is greater
 			}
-			//Calculate the relation between X and Y
-			//and calculate the strechting factor to fit the window
+			// Calculate the relation between X and Y
+			// and calculate the strechting factor to fit the window
 			double latDif = lat2 - lat1; // Breitenunterschied
 			double lonDif = lon2 - lon1; // Höhenunterschied
 
-
-			xMulti = -(-this.xSize / lonDif);
-			yMulti = -(this.ySize / latDif);
+			xMulti = -(-xSize / lonDif);
+			yMulti = -(ySize / latDif);
 
 			double multi = 0;
-			//Make sure that the drawing won't overflow the screen
+			// Make sure that the drawing won't overflow the screen
 			if (xMulti < yMulti) {
 				multi = xMulti;
 			}
@@ -213,15 +197,10 @@ public class MapControle extends StackPane {
 			yMulti = yMulti * zoom;
 
 			clearAndResizeAllLayer(mapLayer, pathLayer, startMarkLayer, destinatinMarkLayer);
-			drawLines(nodeList,
-					mapLayer,
-					false);
+			drawLines(nodeList, mapLayer, false);
 
-			if(pathList != null)
-			{
-				drawLines(pathList,
-						pathLayer,
-						true);
+			if (pathList != null) {
+				drawLines(pathList, pathLayer, true);
 			}
 
 			drawPoint(startNode, startMarkLayer);
@@ -229,65 +208,54 @@ public class MapControle extends StackPane {
 		}
 	}
 
-	void clearAndResizeAllLayer(Canvas ... l){
-		for(Canvas c: l){
+	void clearAndResizeAllLayer(Canvas... l) {
+		for (Canvas c : l) {
 			GraphicsContext graphic = c.getGraphicsContext2D();
-			graphic.getCanvas().setHeight(this.ySize);
-			graphic.getCanvas().setWidth(this.xSize);
+			graphic.getCanvas().setHeight(ySize);
+			graphic.getCanvas().setWidth(xSize);
 			graphic.clearRect(0, 0, xSize, ySize);
 		}
 	}
 
-	void drawPoint(Node n,
-				   Canvas layer)
-	{
-		if(n!=null){
+	void drawPoint(Node n, Canvas layer) {
+		if (n != null) {
 			double highlightDiameter = diameter * 1.5;
 			double nLon = calculateX(n.getLon().doubleValue());
 			double nLat = calculateY(n.getLat().doubleValue());
-			layer.getGraphicsContext2D().fillOval(nLon - highlightDiameter / 2,
-					nLat - highlightDiameter / 2, highlightDiameter,
-					highlightDiameter);
+			layer.getGraphicsContext2D().fillOval(nLon - highlightDiameter / 2, nLat - highlightDiameter / 2,
+					highlightDiameter, highlightDiameter);
 		}
 	};
-	
-	//Draw the lines on the canvas
-	void drawLines(ArrayList<Node> nodes,
-				   Canvas layer,
-				   Boolean highlightPath)
-	{
-		//Set base settings for the canvas
+
+	// Draw the lines on the canvas
+	void drawLines(ArrayList<Node> nodes, Canvas layer, Boolean highlightPath) {
+		// Set base settings for the canvas
 		GraphicsContext graphic = layer.getGraphicsContext2D();
-		
-		//Draw all edges
+
+		// Draw all edges
 		for (Node n : nodes) {
 			double nLon = calculateX(n.getLon().doubleValue());
 			double nLat = calculateY(n.getLat().doubleValue());
 
-			if(!highlightPath && -xMulti > 550){
-				if(n.getJunctionsCount()>2||-xMulti > 450) //graphic.fillText(n.getName(), nLon + 15, nLat);
-				graphic.fillOval(nLon - diameter / 2,
-						nLat - diameter / 2,
-						diameter,
-						diameter);
-				graphic.fillText(n.getName() + " "+n.getShortestDistance(), nLon + 15, nLat);
+			if (!highlightPath && -xMulti > 550) {
+				if (n.getJunctionsCount() > 2 || -xMulti > 450) // graphic.fillText(n.getName(),
+																// nLon + 15,
+																// nLat);
+					graphic.fillOval(nLon - diameter / 2, nLat - diameter / 2, diameter, diameter);
+				graphic.fillText(n.getName() + " " + n.getShortestDistance(), nLon + 15, nLat);
 			}
 
-
-			//Draw each path of the edge
+			// Draw each path of the edge
 			for (Edge e : n.getEdges()) {
 				ArrayList<Node> allNodes = e.allNodes();
 
 				boolean drawPath = !highlightPath;
-				if(highlightPath && (
-						(e.getEndNode() == n && nodes.contains(e.getStartNode())
-						||(e.getStartNode() == n && nodes.contains(e.getEndNode())
-						)
-						))){
+				if (highlightPath && ((e.getEndNode() == n && nodes.contains(e.getStartNode())
+						|| (e.getStartNode() == n && nodes.contains(e.getEndNode()))))) {
 					drawPath = true;
 				}
 
-				if(drawPath){
+				if (drawPath) {
 					for (int i = 0; i < allNodes.size() - 1; i++) {
 						Node en = allNodes.get(i);
 						Node enNext = allNodes.get(i + 1);
@@ -302,11 +270,11 @@ public class MapControle extends StackPane {
 		}
 	}
 
-	double calculateX(double input){
-		return (lon1 -input) * xMulti + this.xSize + xOffset;
+	double calculateX(double input) {
+		return (lon1 - input) * xMulti + xSize + xOffset;
 	}
 
-	double calculateY(double input){
-		return (input - lat1) * yMulti + this.ySize + yOffset;
+	double calculateY(double input) {
+		return (input - lat1) * yMulti + ySize + yOffset;
 	}
 }
